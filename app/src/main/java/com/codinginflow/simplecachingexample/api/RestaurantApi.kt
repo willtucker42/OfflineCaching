f41afcbc3eefda3e53bd7d1c168baf8759653810
willtucker42 <willtucker42@gmail.com>
2021-08-02 17:14:18 -0700

    Initial commit

---
+++ app/src/main/java/com/codinginflow/simplecachingexample/api/RestaurantApi.kt
@@ -0,0 +1,4 @@
+package com.codinginflow.simplecachingexample.api
+
+interface RestaurantApi {
+}
---
+++ app/src/main/java/com/codinginflow/simplecachingexample/data/Restaurant.kt
@@ -0,0 +1,9 @@
+package com.codinginflow.simplecachingexample.data
+
+data class Restaurant (
+    val name: String,
+    val type: String,
+    val logo: String,
+    val address: String
+        ) {
+}
